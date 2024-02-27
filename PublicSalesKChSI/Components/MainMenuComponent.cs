bd83c123a1076d9a99b8a509ace6c73d7eec3594
Лазарина Панева <lazarina.paneva@apis.bg>
2024-02-27 15:34:01 +0200

    MainMenu component Added

---
+++ PublicSalesKChSI/Components/MainMenuComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PublicSalesKChSI.Components
+{
+    public class MainMenuComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {                                           //когато View() е празно, търси Default в папките по конвенция
+            return await Task.FromResult<IViewComponentResult>(View()); //може 2 различни вюта да се подадат, т.е. ще имаме 2 разл вюта
+        }
+    }
+}
