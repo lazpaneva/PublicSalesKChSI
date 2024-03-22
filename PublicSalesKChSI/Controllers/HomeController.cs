1655f64d8edbddafaf56551d927cbb0992913bc7
lazpaneva <lazpaneva@abv.bg>
2024-03-22 16:37:29 +0200

    With Error pages

--- PublicSalesKChSI/Controllers/HomeController.cs
+++ PublicSalesKChSI/Controllers/HomeController.cs
@@ -27,6 +27,19 @@ namespace PublicSalesKChSI.Controllers
             return View();
         }
 
+        public IActionResult Error(int statusCode)
+        {
+            if (statusCode == 400)
+            {
+                return View("Error400");
+            }
+            if (statusCode == 401)
+            {
+                return View("Error400");
+            }
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
